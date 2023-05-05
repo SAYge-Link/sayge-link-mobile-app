deacb523c10fbb289d931c4746a19adb566d8324
Paul Luna <paulluna0215@gmail.com>
2023-05-05 11:51:25 -0700

    adding nav bar to bottom of application

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen'
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false } } name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen options={{ headerShown: false } } name="Login" component={LoginScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,17 @@
+import { KeyboardAvoidingView } from "react-native"
+import {
+    View,
+    Text
+} from 'react-native';
+
+const ProfileScreen = () => {
+    return (
+        <KeyboardAvoidingView>
+            <View>
+                <Text>Profile Page</Text>
+            </View>
+        </KeyboardAvoidingView>
+    )
+};
+
+export default ProfileScreen;
